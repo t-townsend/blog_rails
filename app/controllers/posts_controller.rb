f143ea61ce7270c7d3737d66fad71597a3ae416f
Trista Townsend <tristatownsend@gmail.com>
2017-04-11 16:33:00 -0700

    fixed delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   end
 
    def index
-     @post = Post.new
+     @posts = Post.all
    end
 
    def show
@@ -14,13 +14,34 @@ class PostsController < ApplicationController
      @user = User.find(params[:id])
    end
 
+   def edit
+     @post = Post.find(params[:id])
+   end
+
+   def update
+      post_params = params.require(:post).permit(:title, :body, :image)
+      @post = Post.find params[:id]
+      if @post.update post_params
+        redirect_to post_path(@post)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      post = Post.find params[:id]
+      post.destroy
+      redirect_to posts_path
+    end
+
    def create
      post_params = params.require(:post).permit(:title, :user, :image, :body)
      @post = Post.create post_params
-       if @post.save
+      if @post.save
          redirect_to post_path(@post)
+         render text: "Question created successfully"
        else
        render :new
-      end
+     end
    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
-has_many :comments, dependent: :destroy 
+has_many :comments, dependent: :destroy
+
+validates :title, presence: true
+ validates :body, presence: true
+ validates :image, presence: true
+ 
+
 end
